c2390c046e5a9a228548a9c0f57c3fcba631e586
Yael-Weiss <160965440+Yael-Weiss@users.noreply.github.com>
2025-05-29 20:12:31 +0300

    coord_to_json final

--- coord_to_json.py
+++ coord_to_json.py
@@ -12,7 +12,7 @@ def addresses_to_coords(origin_address, destination_address):
     return origin_point, destination_point
 
 
-def coord_to_json(origin, destination, output_path='routes.json'):
+def coord_to_json(origin, destination):
     # Convert addresses to coordinates
     origin, destination = addresses_to_coords(origin_address, destination_address)
     if not origin or not destination:
@@ -26,15 +26,16 @@ def coord_to_json(origin, destination, output_path='routes.json'):
 
     # Calculate routes
     shortest_path = nx.shortest_path(G, orig_node, dest_node, weight='length')
-    safe_path = nx.shortest_path(G, orig_node, dest_node, weight=lambda u, v, d: edge_safety_weight(u, v, d))
+    alternative_path = nx.shortest_path(G, orig_node, dest_node, weight=lambda u, v, d: edge_safety_weight(u, v, d))
+    
 
-    fig, ax = ox.plot_graph_routes(G, [shortest_path, safe_path], route_linewidth=6, node_size=0)
-    plt.show()
+    # fig, ax = ox.plot_graph_routes(G, [shortest_path, alternative_path],route_colors=['#FF0000', '#00FF00'], route_linewidth=6, node_size=0)
+    # plt.show()
 
 
     # Build route geometries
     routes = []
-    for idx, path in enumerate([shortest_path, safe_path]):
+    for idx, path in enumerate([shortest_path, alternative_path]):
         coords = []
         for node in path:
             point = G.nodes[node]
@@ -57,12 +58,7 @@ def coord_to_json(origin, destination, output_path='routes.json'):
         "routes": routes
     }
 
-    # Save to file
-    with open(output_path, 'w') as f:
-        json.dump(output, f, indent=2)
-
-    print(f"JSON saved to {output_path}")
-
+    return json.dumps(output)    
 
 # function to calculate safety score per edge
 def edge_safety_weight(u, v, data):
@@ -81,10 +77,11 @@ def edge_safety_weight(u, v, data):
 
 
 if __name__ == "__main__":
-    origin_address = "Bugrashov Beach Tel Aviv"        # Bugrashov Beach
-    destination_address = "Sarona Market Tel Aviv"   # Sarona Market
+    origin_address = "Jaffa Flea Market Tel Aviv"        # Bugrashov Beach
+    destination_address = "Carmel Market Tel Aviv"   # Sarona Market
     output_path = 'routes.json'
 
-    coord_to_json(origin_address, destination_address, output_path)
+    json_obj = coord_to_json(origin_address, destination_address)
+    print(f"JSON output:\n{json_obj}")
     
     
